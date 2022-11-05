5d376b8a7d97fb8e374b38659eca2c86ee3d15bd
Elledgef <elledgef@oregonstate.edu>
2022-11-05 15:23:18 -0700

    edit

--- list_max.py
+++ list_max.py
@@ -5,13 +5,34 @@
 
 def list_max(num_list):
     """ This function takes a list of numbers as a parameter and outputs the maximum value in the list """
-    if len(num_list) == 1:
-        return num_list[0]
+    if len(num_list) <= 1:
+            return num_list[0]
     else:
-        return_result = list_max(num_list[1:])
-        return return_result
-        if return_result <= num_list[0]:
-           return_result = num_list[0]
+        return_value = list_max(num_list[1:])
+        return return_value \
+            if return_value > num_list[0] \
+                else num_list [0]
+
+
+
+
+
+
+
+
+
+
+num_list = [1,3,100,4]
+print(str(list_max(num_list)))
+
+
+
+
+num_list = [1,3,100,2]
+print(list_max(num_list))
+
+
+
 
 
 
